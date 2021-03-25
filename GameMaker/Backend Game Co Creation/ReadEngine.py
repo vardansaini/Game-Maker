2c901c5fe32a80fb56024486a36aac937eb2f739
Vardan Saini <Vardan1@ualberta.ca>
2021-03-24 18:56:02 -0600

    Read Engine fixed and slight upgrades

--- GameMaker/Backend Game Co Creation/ReadEngine.py
+++ GameMaker/Backend Game Co Creation/ReadEngine.py
@@ -4,7 +4,7 @@ from engine import *
 import random
 import pickle
 
-engine = pickle.load(open("finalLearnedEngine.p", "rb"))
+engine = pickle.load(open("./finalLearnedEngine.p", "rb"))
 
 ruleNum = 0
 for rule in engine.rules:
